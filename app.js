339f4e6eef60f240f5562de45dd3ea7dda9832fc
Joan Jimenez <jimenez.donaldson@gmail.com>
2023-07-26 14:19:40 -0400

    Created calculator UI

---
+++ app.js
@@ -0,0 +1,41 @@
+// 1. Create basic calculator functions
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+// 2. Create operation variables
+
+let num1;
+let num2;
+let operator;
+
+// 3. Create function to operate on two numbers
+
+function operate(num1, num2, operator) {
+  switch (operator) {
+    case '+':
+      return add(num1, num2);
+
+    case '-':
+      return subtract(num1, num2);
+
+    case '*':
+      return multiply(num1, num2);
+
+    case '/':
+      return divide(num1, num2);
+  }
+}
